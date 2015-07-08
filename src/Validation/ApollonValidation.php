c04e17a0d7beebe3fcf09b5bf4c42a4d7b95cc32
Satoru Hagiwara <hagiwara@fusic.co.jp>
2015-07-08 15:22:24 +0900

    数値チェックについてintの上限値を同時にチェックするよう修正

--- src/Validation/ApollonValidation.php
+++ src/Validation/ApollonValidation.php
@@ -12,7 +12,7 @@ class ApollonValidation extends Validation
      * @access public
      * @author hagiwara
      */
-    public static function zipCheck($check, $context)
+    public static function zipCheck($check)
     {
         $regex = '/^[0-9]{3}-?[0-9]{4}$/';
         return self::_check($check, $regex);
@@ -25,7 +25,7 @@ class ApollonValidation extends Validation
      * @access public
      * @author hagiwara
      */
-    public static function zip1Check($check, $context)
+    public static function zip1Check($check)
     {
         $regex = '/^[0-9]{3}$/';
         return self::_check($check, $regex);
@@ -38,7 +38,7 @@ class ApollonValidation extends Validation
      * @access public
      * @author hagiwara
      */
-    public static function zip2Check($check, $context)
+    public static function zip2Check($check)
     {
         $regex = '/^[0-9]{4}$/';
         return self::_check($check, $regex);
@@ -50,10 +50,57 @@ class ApollonValidation extends Validation
      * @access public
      * @author sakuragawa
      */
-    public static function alpha($check, $context)
+    public static function alpha($check)
     {
         $regex = '/^[a-zA-Z]+$/u';
         return self::_check($check, $regex);
+    }
+    
+    /**
+     * numericCheck
+     * 数値チェック
+     * integerなどの上限チェックを同時に行う
+     *
+     * @access public
+     * @author hagiwara
+     */
+    public static function numericCheck($check, $limit = 2147483647)
+    {
+        //providersが間違いなく$contextの内容と考えられるので初期値を入力しなおす
+        if (is_array($limit) && isset($limit['providers'])){
+            $limit = 2147483647;
+        }
+        
+        //coreのチェックを先に行う
+        if (!parent::numeric($check)){
+            return false;
+        }
+        return abs($check) <= $limit;
+    }
+    
+    /**
+     * naturalNumber
+     * 数値チェック
+     * integerなどの上限チェックを同時に行う
+     *
+     * @access public
+     * @author hagiwara
+     */
+    public static function naturalNumber($check, $allowZero = false, $limit = 2147483647)
+    {
+        //providersが間違いなく$contextの内容と考えられるので初期値を入力しなおす
+        if (is_array($allowZero) && isset($allowZero['providers'])){
+            $allowZero = false;
+        }
+        if (is_array($limit) && isset($limit['providers'])){
+            $limit = 2147483647;
+        }
+        
+        //coreのチェックを先に行う
+        if (!parent::naturalNumber($check, $allowZero)){
+            return false;
+        }
+        return abs($check) <= $limit;
     }
 }
 
--- tests/TestCase/Validation/ApollonValidationTest.php
+++ tests/TestCase/Validation/ApollonValidationTest.php
@@ -32,43 +32,75 @@ class ApollonValidationTest extends TestCase
     }
 
     /**
-     * testNotEmpty method
+     * test_zipCheck method
      *
      * @return void
      */
     public function test_zipCheck()
     {
-        $this->assertTrue(ApollonValidation::zipCheck('810-0001',[]));
-        $this->assertTrue(ApollonValidation::zipCheck('8100001',[]));
-        $this->assertFalse(ApollonValidation::zipCheck('810000',[]));
-        $this->assertFalse(ApollonValidation::zipCheck('8100-001',[]));
-        $this->assertFalse(ApollonValidation::zipCheck('810-000a',[]));
+        $this->assertTrue(ApollonValidation::zipCheck('810-0001'));
+        $this->assertTrue(ApollonValidation::zipCheck('8100001'));
+        $this->assertFalse(ApollonValidation::zipCheck('810000'));
+        $this->assertFalse(ApollonValidation::zipCheck('8100-001'));
+        $this->assertFalse(ApollonValidation::zipCheck('810-000a'));
     }
 
     /**
-     * testNotEmpty method
+     * test_zip1Check method
      *
      * @return void
      */
     public function test_zip1Check()
     {
-        $this->assertTrue(ApollonValidation::zip1Check('810',[]));
-        $this->assertFalse(ApollonValidation::zip1Check('8100001',[]));
-        $this->assertFalse(ApollonValidation::zip1Check('810-0001',[]));
-        $this->assertFalse(ApollonValidation::zip1Check('81a',[]));
+        $this->assertTrue(ApollonValidation::zip1Check('810'));
+        $this->assertFalse(ApollonValidation::zip1Check('8100001'));
+        $this->assertFalse(ApollonValidation::zip1Check('810-0001'));
+        $this->assertFalse(ApollonValidation::zip1Check('81a'));
     }
 
     /**
-     * testNotEmpty method
+     * test_zip2Check method
      *
      * @return void
      */
     public function test_zip2Check()
     {
-        $this->assertTrue(ApollonValidation::zip2Check('0001',[]));
-        $this->assertFalse(ApollonValidation::zip2Check('8100001',[]));
-        $this->assertFalse(ApollonValidation::zip2Check('810-0001',[]));
-        $this->assertFalse(ApollonValidation::zip2Check('000a',[]));
+        $this->assertTrue(ApollonValidation::zip2Check('0001'));
+        $this->assertFalse(ApollonValidation::zip2Check('8100001'));
+        $this->assertFalse(ApollonValidation::zip2Check('810-0001'));
+        $this->assertFalse(ApollonValidation::zip2Check('000a'));
+    }
+
+    /**
+     * test_numericCheck method
+     *
+     * @return void
+     */
+    public function test_numericCheck()
+    {
+        $this->assertTrue(ApollonValidation::numericCheck('12345'));
+        $this->assertTrue(ApollonValidation::numericCheck('2147483647'));
+        $this->assertTrue(ApollonValidation::numericCheck('-2147483647'));
+        $this->assertFalse(ApollonValidation::numericCheck('2147483648'));
+        $this->assertFalse(ApollonValidation::numericCheck('-2147483648'));
+        $this->assertTrue(ApollonValidation::numericCheck('12345', '20000'));
+        $this->assertFalse(ApollonValidation::numericCheck('12345', '12344'));
+    }
+
+    /**
+     * test_numericCheck method
+     *
+     * @return void
+     */
+    public function test_naturalNumber()
+    {
+        $this->assertTrue(ApollonValidation::naturalNumber('12345'));
+        $this->assertTrue(ApollonValidation::naturalNumber('2147483647'));
+        $this->assertFalse(ApollonValidation::naturalNumber('-2147483647'));
+        $this->assertFalse(ApollonValidation::naturalNumber('2147483648'));
+        $this->assertFalse(ApollonValidation::naturalNumber('-2147483648'));
+        $this->assertTrue(ApollonValidation::naturalNumber('12345', false, '20000'));
+        $this->assertFalse(ApollonValidation::naturalNumber('12345', false, '12344'));
     }
 
 }
